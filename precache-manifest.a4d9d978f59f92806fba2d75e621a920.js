5f27c900f48c6cf59ae6c98d1b6e704fa053b021
Tu Dat Nguyen <tudat@affinitybridge.com>
2020-05-13 16:20:05 -0700

    deploy

---
+++ precache-manifest.a4d9d978f59f92806fba2d75e621a920.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27011215d2eb3b75def674217108263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8e7a43cc40aafc80b12",
+    "url": "/static/css/main.7ee92869.chunk.css"
+  },
+  {
+    "revision": "f9ffdef3297a67e36c88",
+    "url": "/static/js/2.de8c3b12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de8c3b12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e7a43cc40aafc80b12",
+    "url": "/static/js/main.fcafaf1d.chunk.js"
+  },
+  {
+    "revision": "f918322b275118b1d3cd",
+    "url": "/static/js/runtime-main.e986bfcd.js"
+  },
+  {
+    "revision": "c3d00838ee827eea7a693a15d44f9d11",
+    "url": "/static/media/C.c3d00838.png"
+  },
+  {
+    "revision": "38548bda6a3b5a89024ec5eec2e7722a",
+    "url": "/static/media/beatbox.38548bda.png"
+  },
+  {
+    "revision": "86f69c2a0b12c9d22f128463421dab16",
+    "url": "/static/media/candy.86f69c2a.png"
+  },
+  {
+    "revision": "551ad592a7fcb2d80c1e8dba1b5b2c43",
+    "url": "/static/media/dreambedded.551ad592.png"
+  },
+  {
+    "revision": "98ff4822d03ab0f5746653f56441d38e",
+    "url": "/static/media/expressjs.98ff4822.png"
+  },
+  {
+    "revision": "72aea34651f6ae0561814f75b398470d",
+    "url": "/static/media/geobox.72aea346.png"
+  },
+  {
+    "revision": "eee3311ab0e935fbbfcad68467d6dd45",
+    "url": "/static/media/justwrite.eee3311a.png"
+  },
+  {
+    "revision": "1c4d038e93595d208f028f68821f0396",
+    "url": "/static/media/logo.1c4d038e.png"
+  },
+  {
+    "revision": "1ecd21a8b75362f946fd0ec2bcc369a0",
+    "url": "/static/media/rails.1ecd21a8.png"
+  },
+  {
+    "revision": "57a972fceb7bb7549d0a7da80aed1d7b",
+    "url": "/static/media/react_icon.57a972fc.png"
+  },
+  {
+    "revision": "88f7cec51db23d9231ef9e36643efc79",
+    "url": "/static/media/sfu.88f7cec5.jpg"
+  },
+  {
+    "revision": "a0c5bb4c4bb1977f6d1df03fc54d728d",
+    "url": "/static/media/simulatER.a0c5bb4c.png"
+  },
+  {
+    "revision": "61e92a0e0e58104ef5da592656ffc46b",
+    "url": "/static/media/sparty_large.61e92a0e.png"
+  },
+  {
+    "revision": "315d1429d1630c8548a2f49ce02a1166",
+    "url": "/static/media/tetris.315d1429.gif"
+  },
+  {
+    "revision": "345578ca42c36f23f346cb0ca6fdb9ad",
+    "url": "/static/media/unity.345578ca.png"
+  }
+]);
